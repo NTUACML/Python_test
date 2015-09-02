3f5b2784567373b176fde7c37de34189d6aa7c08
kavy7884 <chingkaichou@gmail.com>
2015-09-02 12:56:36 +0800

    First Push

---
+++ hello.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+print("hello","world!")
+
+route = 886
+print(route, type(route))
+
+a = "kavy","yo"
+b = ["Go", 1, "GG", a]
+c = "one",
+print(a)
+print(b)
+print(c)
+
+print(len(a))
+print(len(b))
+print(len("automatically"))
+print(type(a))
+print(type(b))
+
+b.append("more")
+print(b)
+list.append(b,"NaNa")
+print(b)
+
+print(b[3])
+b[3] = "User"
+print(b)
+
+d = ["a","b","c"]
+e=d;
+print(d is e, e is not None)
+f=["a","b","c"]
+print(d is f, d==f)
+
+print("GG" in b)
+print(2 in c)
